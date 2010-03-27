78b04baa9419be42fc040ef80a189ecf84bbe1f3
Ethan Shepherd <someguy235@gmail.com>
2010-03-27 10:44:49 -0400

    started passing params --ems

--- grails-app/controllers/com/lastFMGenreStats/UserController.groovy
+++ grails-app/controllers/com/lastFMGenreStats/UserController.groovy
@@ -8,6 +8,11 @@ class UserController {
 		}
 
 		def results = {
-			render "<h1>Success!</h1>"
+			render "user: ${params.username} <br />"
+			def artistPlays = [:]
+			artistPlays.put("test1", 1)
+			artistPlays.put("test2", 2)
+			render "test?: ${artistPlays.get("test1")}<br />"
+			render "test?: ${artistPlays.get("test2")}<br />"
 		}
 }
---
+++ grails-app/views/user/results.gsp
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<title>Last.fm Genre Results for </title>
+		<meta name="layout" content="main"/>
+	</head>
+	<body>
+		<h1>Results</h1>
+		<p>
+		</p>
+		<ul>
+		</ul>
+		<g:link action='index'>Search Again</g:link>
+	</body>
+</html>
+
