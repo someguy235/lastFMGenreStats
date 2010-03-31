fa289dc7987bf2639ebfcf4969a866da3811e0da
Ethan Shepherd <someguy235@gmail.com>
2010-03-30 20:51:39 -0400

    added some jquery --ems

--- grails-app/controllers/com/lastFMGenreStats/UserController.groovy
+++ grails-app/controllers/com/lastFMGenreStats/UserController.groovy
@@ -6,8 +6,10 @@ class UserController {
 		}
 
 		def results = {
+			def APIkey = "503475a6b1d2cafdedf2f69ed4f677c3"
 			def APIroot = "http://ws.audioscrobbler.com/2.0/"
-			def topArtistsURL = APIroot + "?method=user.gettopartists&user=${params.username}&period=${params.period}&api_key=503475a6b1d2cafdedf2f69ed4f677c3"
+
+			def topArtistsURL = "${APIroot}?method=user.gettopartists&user=${params.username}&period=${params.period}&api_key=${APIkey}"
 			def topArtistsRESTResponse = new URL(topArtistsURL).getText()
 			def topArtistsXML = new XmlSlurper().parseText(topArtistsRESTResponse)
 			def topArtistsList = topArtistsXML.topartists.artist
@@ -15,16 +17,31 @@ class UserController {
 			render "user: ${params.username} <br />"
 			render "period: ${params.period} <br />"
 			
-			//def artistPlays = [:]
+			def tags = [:]
 			//artistPlays.put("test1", 1)
 			//render "test?: ${artistPlays.get("test1")}<br />"
 			
 			render "<br />"
-			for (artist in 0..(topArtistsList.size()-1)) {
-				render "${topArtistsXML.topartists.artist[artist].@rank.text()}:\t"
-				render "${topArtistsXML.topartists.artist[artist].name.text()}"
-				render " (${topArtistsXML.topartists.artist[artist].playcount.text()})<br />"
-			
+			//for (artist in 0..(topArtistsList.size()-1)) {
+			for (artist in 0..5) {
+				def artistRank 	= topArtistsXML.topartists.artist[artist].@rank.text()
+				def artistName 	= topArtistsXML.topartists.artist[artist].name.text()
+				def artistPlays = topArtistsXML.topartists.artist[artist].playcount.text()
+				//render "${artistRank}: ${artistName} (${artistPlays}) <br />"
+				def artistURL 	= "${APIroot}?method=artist.gettoptags&artist=${artistName}&api_key=${APIkey}"				
+				def artistRESTResponse = new URL(artistURL).getText()
+				def artistXML 	= new XmlSlurper().parseText(artistRESTResponse)
+				//render "${artistXML}"
+				def artistTags	= artistXML.toptags.tag
+				//render "${artistTags}"
+				for (tag in 1..5){
+					def tagName = artistXML.toptags.tag[tag].name.text()
+					def tagCount = artistXML.toptags.tag[tag].count.text()
+					tags.put(tagName, tagCount)
+					//render "&nbsp&nbsp ${tagName}: ${tagCount} <br />" 
+				}
+				sleep 1 
 			}
+			render "${tags.toString()}"
 		}
 }
--- grails-app/views/user/index.gsp
+++ grails-app/views/user/index.gsp
@@ -1,22 +1,33 @@
-<formset>
-	<legend>Enter a Username</legend>
-	<g:form action="results">
-		<label for="username">Username</label>
-		<g:textField name="username" />
-		<br />
-		<label for="period">Period</label>
-		<br />
-		<g:radio name="period" value="Overall" checked="true" />
-		Overall <br />
-		<g:radio name="period" value="7day" />
-		7 Days <br />
-		<g:radio name="period" value="3month" />
-		3 Months <br />
-		<g:radio name="period" value="6month" />
-		6 Months <br />
-		<g:radio name="period" value="12month" />
-		12 Months <br /><br />
-		<g:submitButton name="search" value="Submit"/>
-	</g:form>
-</formset>
-
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+		<title>Last.fm Genre Stats</title>
+		<link type="text/css" href="css/custom-theme/jquery-ui-1.8.custom.css" rel="stylesheet" />	
+		<script type="text/javascript" src="js/jquery-1.4.2.min.js"></script>
+		<script type="text/javascript" src="js/jquery-ui-1.8.custom.min.js"></script>
+	</head>
+	<body>
+		<formset>
+			<legend>Enter a Username</legend>
+			<g:form action="results">
+				<label for="username">Username</label>
+				<g:textField name="username" />
+				<br />
+				<label for="period">Period</label>
+				<br />
+				<g:radio name="period" value="Overall" checked="true" />
+				Overall <br />
+				<g:radio name="period" value="7day" />
+				7 Days <br />
+				<g:radio name="period" value="3month" />
+				3 Months <br />
+				<g:radio name="period" value="6month" />
+				6 Months <br />
+				<g:radio name="period" value="12month" />
+				12 Months <br /><br />
+				<g:submitButton name="search" value="Submit"/>
+			</g:form>
+		</formset>
+	</body>
+</html>
